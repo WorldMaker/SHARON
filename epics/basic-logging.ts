66b592bc09d991633cabba38f961a76875a2ba27
Max Battcher <me@worldmaker.net>
2019-01-02 09:57:45 -0500

    ✒ Buffer basic log messages
    To better limit the rate of reporting, group and buffer basic log
    messages before sending.

--- epics/basic-logging.ts
+++ epics/basic-logging.ts
@@ -1,9 +1,11 @@
 import { StateObservable } from 'redux-observable'
 import { Observable, from } from 'rxjs'
-import { map, concatMap, ignoreElements } from 'rxjs/operators'
+import { map, concatMap, ignoreElements, groupBy, mergeMap, bufferTime } from 'rxjs/operators'
 import { Action, ActionType } from '../actions'
 import { Store } from '../store'
 import { DiscordDependency, log } from './model'
+
+export const BasicLogInterval = 2 /* s */ * 1000 /* ms */
 
 function basicLogMessage (action: Action) {
   switch (action.type) {
@@ -34,6 +36,11 @@ function basicLogMessage (action: Action) {
 export default function basicLoggingEpic (action: Observable<Action>, _state: StateObservable<Store>, { client }: DiscordDependency) {
   return action.pipe(
     map(basicLogMessage),
+    groupBy(([guildId]) => guildId, ([, message]) => message),
+    mergeMap(group => group.pipe(
+      bufferTime(BasicLogInterval),
+      map(buffer => [group.key, buffer.join('\n')])
+    )),
     concatMap(([guildId, message]) => from(log(client, guildId, message))),
     ignoreElements()
   )
