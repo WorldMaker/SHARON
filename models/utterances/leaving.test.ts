b5453ead39ba0a684fc23bc9a74f26273c980dc4
Max Battcher <me@worldmaker.net>
2019-01-29 01:48:07 -0500

    👄 First pass of leaving utterance model (#5)

---
+++ models/utterances/leaving.test.ts
@@ -0,0 +1,14 @@
+import examineLeaving from './leaving'
+
+test('leaving corpus', () => {
+  const test1 = examineLeaving('leaving')
+  expect(test1).toBeTruthy()
+  expect(test1 && test1.personal).toBeFalsy()
+  expect(test1 && test1.negative).toBeFalsy()
+  const test2 = examineLeaving('I am leaving')
+  expect(test2 && test2.personal).toBeTruthy()
+  expect(test2 && test2.negative).toBeFalsy()
+  const test3 = examineLeaving('I\'m not leaving')
+  expect(test3 && test3.personal).toBeTruthy()
+  expect(test3 && test3.negative).toBeTruthy()
+})
---
+++ models/utterances/leaving.ts
@@ -0,0 +1,53 @@
+import { nlp } from '../index'
+
+export enum LeavingType {
+  Now = 'now',
+  Soon = 'soon',
+  At = 'at'
+}
+
+export interface LeavingNow {
+  type: LeavingType.Now
+}
+
+export interface LeavingSoon {
+  type: LeavingType.Soon
+}
+
+export interface LeavingAt {
+  type: LeavingType.At
+  time: string
+}
+
+export type LeavingWhen = LeavingNow | LeavingSoon | LeavingAt
+
+export interface LeavingUtterance {
+  utterance: string
+  negative: boolean
+  personal: boolean
+  when?: LeavingWhen
+}
+
+export default function examineForLeaving (utterance: string, doc: any = null): LeavingUtterance | null {
+  if (!doc) {
+    doc = (nlp as any)(utterance)
+  }
+
+  if (!doc.has('(go|going|leave|leaving)')) {
+    return null
+  }
+
+  const verbs = doc.verbs()
+  const negative = verbs.isNegative().found
+  const personal = doc.has('(i|i\'m)')
+
+  let when: LeavingWhen | undefined = undefined
+
+  if (doc.has('(now|immediately)')) {
+    when = { type: LeavingType.Now }
+  } else if (doc.has('(soon|after this|after voyage)')) {
+    when = { type: LeavingType.Soon }
+  }
+
+  return { utterance, negative, personal, when }
+}
